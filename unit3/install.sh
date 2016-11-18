9522be87c546d0ba949dda757628e7bb77b7bf05
Michael Wittig <post@michaelwittig.info>
2016-11-18 08:32:50 +0100

    added install steps

---
+++ unit3/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+
+sudo su
+
+yum update
+
+yum install httpd24 php56 php56-mysqlnd mysql56
+
+chkconfig httpd on
+
+cd /var/www/html
+
+wget https://github.com/AWSinMotion/Twitter-Like-System-PHP/archive/master.zip
+
+unzip -j master.zip
+
+cat > /etc/httpd/conf.d/awsinmotion.conf << EOF
+<Directory "/var/www/html">
+    AllowOverride All
+</Directory>
+EOF
+
+service httpd start
